77eca5195cb917e7f2dfd945cd358c81a4a57b91
Maxim Zaikin <mxmzkn19@gmail.com>
2025-11-06 16:14:22 +0200

    Lb6: changing the style structure, converting CSS to SCSS, updating main.scss

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig  } from "vite";
-import { resolve } from "node:path";
+import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url'
+import handlebars from 'vite-plugin-handlebars'
 
-export default defineConfig( { 
-    resolve: { alias : { '@': resolve(__dirname, 'src') } },
-    build:  {  
-        rollupOptions: { 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = dirname(__filename)
+
+export default defineConfig({
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src', 'partials'),
+            context: { siteName: 'Лабораторна 6', labName: 'Лабораторна № 6' },
+            reloadOnPartialChange: true
+        }),
+    ],
+    resolve: { alias: { '@': resolve(__dirname, 'src') } },
+    build: {
+        rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'about.html'),
+                main:     resolve(__dirname, 'index.html'),
+                about:    resolve(__dirname, 'about.html'),
                 contacts: resolve(__dirname, 'contacts.html'),
-            }
-        } 
-    }
-} );
-        
+            },
+        },
+    },
+});
